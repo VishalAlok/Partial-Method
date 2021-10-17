6579e32a8d33bb66c48255912023c67e12c6ea2b
vishal.Alok <you@example.com>
2021-10-17 22:43:40 +0530

    21st

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Partial_Method
+{
+    partial class A
+    {
+        partial void OnSomethingHappened(string s);
+    }
+    // This part can be in a separate file.   
+    partial class A
+    {
+        // Comment out this method and the program   
+        // will still compile.   
+        partial void OnSomethingHappened(String s)
+        {
+            Console.WriteLine("Something happened: {0}", s);
+        }
+    }
+}
